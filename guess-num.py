664dde1e32b5831989fe0a6c7abf49209fcefaeb
Oscar Choi <oscarchoi.hk@gmail.com>
2020-07-20 21:03:11 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -1,17 +1,21 @@
 import random
 
-r = random.randint(1, 100)
 x = 0
+y = input('Please enter the minimum value of the random number range!')
+z = input('Please enter the maximuim value of the random number range!')
+y = int(y)
+z = int(z)
+r = random.randint(y, z)
 while True:
-	guess = input('guess a number from 1-100!')
+	guess = input('Guess a number!')
 	guess = int(guess)
 	x = x + 1
 	if r == guess:
 		print('Correct number!')
-		print('you guessed', x,'times!')
+		print('You guessed', x,'times!')
 		break
 	elif r > guess:
 		print('Too small!')
 	elif r < guess:
-		print('too large!')
-	print('you guessed', x,'times!')
+		print('Too large!')
+	print('You guessed', x,'times!')
